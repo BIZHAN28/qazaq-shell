fc8e85a0462d96019221a04bdd8e1b9ac9813bd3
BIZHAN28 <bizhanov.rasul@mail.ru>
2025-01-22 10:27:38 +0300

    feat: color update

--- shell.c
+++ shell.c
@@ -1,186 +1,217 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/time.h>
 #include <time.h>
 
 #define MAX_INPUT 1024
-#define MAX_COMMANDS 10
+#define MAX_ARGS 64
+#define MAX_HEALTH 100
+
+// ANSI escape codes для цветного текста
+#define RESET       "\033[0m"
+#define RED         "\033[31m"
+#define GREEN       "\033[32m"
+#define YELLOW      "\033[33m"
+#define BLUE        "\033[34m"
+#define MAGENTA     "\033[35m"
+#define CYAN        "\033[36m"
+#define WHITE       "\033[37m"
 
 typedef struct {
-    char name[64];
+    char *name;
     int health;
     int damage;
-    int coins_reward;
-    float effect_chance;
-} Enemy;
+    int coin_reward;
+    int effect_chance;
+} Monster;
 
-typedef struct {
-    char name[64];
-    int cost;
-    int unlocked;
-} Command;
-
-Command available_commands[MAX_COMMANDS];
-int command_count = 0;
-int player_health = 100;
+// Глобальные переменные для игры
+int player_health = MAX_HEALTH;
 int player_coins = 0;
-int player_frozen = 0;    // Состояние "заморожен"
-int player_burning = 0;   // Состояние "горит"
+int game_running = 0;
 
-// Инициализация доступных команд
-void initialize_commands() {
-    strcpy(available_commands[0].name, "ls");
-    available_commands[0].cost = 10;
-    available_commands[0].unlocked = 0;
-
-    strcpy(available_commands[1].name, "cat");
-    available_commands[1].cost = 15;
-    available_commands[1].unlocked = 0;
-
-    strcpy(available_commands[2].name, "echo");
-    available_commands[2].cost = 5;
-    available_commands[2].unlocked = 0;
-
-    command_count = 3;
+// Функции игры
+void print_welcome_message() {
+    printf(GREEN "Добро пожаловать в подземелье!\n");
+    printf("В вашем распоряжении три этажа: Ледяной, Огненный и Пустынный.\n");
+    printf("Бой с монстрами принесет вам монеты, которые вы можете использовать для разблокировки команд!\n\n");
 }
 
-// Генерация случайного врага
-Enemy generate_enemy(char *location) {
-    Enemy enemy;
-    int type = rand() % 3; // 0: Миньон, 1: Элементаль, 2: Обычный монстр
-
-    if (type == 0) {
-        strcpy(enemy.name, "Миньон");
-        enemy.health = 20 + rand() % 10;
-        enemy.damage = 5 + rand() % 5;
-        enemy.coins_reward = 5 + rand() % 5;
-        enemy.effect_chance = strcmp(location, "Ледяная") == 0 ? 0.2 : strcmp(location, "Огненная") == 0 ? 0.1 : 0.05;
-    } else if (type == 1) {
-        strcpy(enemy.name, "Элементаль");
-        enemy.health = 25 + rand() % 10;
-        enemy.damage = 10 + rand() % 5;
-        enemy.coins_reward = 10 + rand() % 5;
-        enemy.effect_chance = strcmp(location, "Ледяная") == 0 ? 0.5 : strcmp(location, "Огненная") == 0 ? 0.3 : 0.1;
-    } else {
-        strcpy(enemy.name, "Обычный монстр");
-        enemy.health = 30 + rand() % 10;
-        enemy.damage = 7 + rand() % 7;
-        enemy.coins_reward = 8 + rand() % 7;
-        enemy.effect_chance = strcmp(location, "Ледяная") == 0 ? 0.35 : strcmp(location, "Огненная") == 0 ? 0.2 : 0.07;
-    }
-
-    return enemy;
+void print_game_status() {
+    printf("\n" CYAN "Здоровье игрока: %d, Монеты: %d\n" RESET, player_health, player_coins);
 }
 
-// Покупка команды
-void buy_command(char *command) {
-    for (int i = 0; i < command_count; i++) {
-        if (strcmp(available_commands[i].name, command) == 0) {
-            if (available_commands[i].unlocked) {
-                printf("Команда %s уже разблокирована!\n", command);
-                return;
-            }
-            if (player_coins >= available_commands[i].cost) {
-                player_coins -= available_commands[i].cost;
-                available_commands[i].unlocked = 1;
-                printf("Команда %s разблокирована!\n", command);
-            } else {
-                printf("Недостаточно монет для разблокировки команды %s!\n", command);
-            }
-            return;
+void game_over() {
+    printf(RED "\nВы погибли! Игра закончена.\n" RESET);
+    game_running = 0;
+}
+
+void fight_monster(Monster monster) {
+    printf(MAGENTA "Вы встретили монстра: %s! Бой начинается!\n" RESET, monster.name);
+    printf("Здоровье монстра: %d, Урон: %d, Награда за убийство: %d монет.\n", monster.health, monster.damage, monster.coin_reward);
+
+    // Сражение
+    while (monster.health > 0 && player_health > 0) {
+        printf("\nВаш ход: (1) Атака (2) Пропустить ход\n");
+        char choice;
+        scanf(" %c", &choice);
+
+        if (choice == '1') {
+            printf("Вы атаковали монстра!\n");
+            monster.health -= rand() % 20 + 10;  // Урон от игрока
         }
-    }
-    printf("Команда %s не существует!\n", command);
-}
 
-// Сражение с врагом
-void fight_enemy(Enemy enemy, char *location) {
-    printf("Вы встретили врага: %s (Здоровье: %d, Урон: %d)!\n", enemy.name, enemy.health, enemy.damage);
+        if (monster.health <= 0) {
+            printf(GREEN "Монстр повержен! Вы получили %d монет.\n" RESET, monster.coin_reward);
+            player_coins += monster.coin_reward;
+            break;
+        }
 
-    while (enemy.health > 0 && player_health > 0) {
-        if (player_frozen) {
-            printf("Вы заморожены и пропускаете ход!\n");
-            player_frozen = 0; // Заморозка снимается после одного хода
+        // Ответный ход монстра
+        if (rand() % 100 < monster.effect_chance) {
+            if (strcmp(monster.name, "Ледяной Миньон") == 0) {
+                printf(RED "Монстр замораживает вас! Вы пропустили ход.\n" RESET);
+            } else if (strcmp(monster.name, "Огненный Элементаль") == 0) {
+                printf(RED "Вы горите! Получите урон от огня.\n" RESET);
+                player_health -= rand() % 10 + 5;
+            } else if (strcmp(monster.name, "Пустынный Монстр") == 0) {
+                printf(RED "Монстр убегает от вас!\n" RESET);
+                break;
+            }
         } else {
-            printf("Ваше здоровье: %d\n", player_health);
-            printf("Здоровье врага: %d\n", enemy.health);
-            printf("1. Атаковать\n");
-            printf("2. Убежать\n");
-            printf("Выберите действие: ");
-            int choice;
-            scanf("%d", &choice);
-
-            if (choice == 1) {
-                int damage = 10 + rand() % 5;
-                printf("Вы нанесли %d урона врагу!\n", damage);
-                enemy.health -= damage;
-
-                if (strcmp(location, "Огненная") == 0 && rand() % 100 < (int)(enemy.effect_chance * 100)) {
-                    player_burning = 5 + rand() % 5; // Игрок "горит"
-                    printf("Вы горите! В следующий ход получите %d урона.\n", player_burning);
-                }
-            } else if (choice == 2) {
-                printf("Вы сбежали из боя!\n");
-                return;
-            } else {
-                printf("Неверный выбор!\n");
-            }
+            printf("Монстр промахнулся!\n");
         }
 
-        // Атака врага
-        if (enemy.health > 0) {
-            player_health -= enemy.damage;
-            printf("Враг атаковал вас и нанёс %d урона!\n", enemy.damage);
-
-            if (strcmp(location, "Ледяная") == 0 && rand() % 100 < (int)(enemy.effect_chance * 100)) {
-                player_frozen = 1; // Игрок "заморожен"
-                printf("Вы заморожены и пропустите следующий ход!\n");
-            }
+        if (player_health <= 0) {
+            game_over();
+            break;
         }
 
-        // Урон от горения
-        if (player_burning > 0) {
-            printf("Вы получили %d урона от горения!\n", player_burning);
-            player_health -= player_burning;
-            player_burning = 0; // Эффект горения снимается
-        }
-    }
-
-    if (player_health > 0) {
-        printf("Вы победили врага и получили %d монет!\n", enemy.coins_reward);
-        player_coins += enemy.coins_reward;
-    } else {
-        printf("Вы погибли! Игра окончена.\n");
-        exit(0);
+        print_game_status();
     }
 }
 
-// Основная функция игры
-void dungeon_crawler() {
-    char *locations[] = {"Ледяная", "Огненная", "Пустынная"};
-    while (player_health > 0) {
-        printf("Выберите уровень подземелья:\n");
-        printf("1. Ледяная\n2. Огненная\n3. Пустынная\n");
-        printf("Ваш выбор: ");
+void start_game() {
+    game_running = 1;
+    srand(time(NULL));  // Инициализация генератора случайных чисел
+
+    print_welcome_message();
+
+    // Игровой цикл
+    while (game_running) {
+        printf("Вы находитесь в подземелье. Выберите этаж:\n");
+        printf("1) Ледяной Этаж\n");
+        printf("2) Огненный Этаж\n");
+        printf("3) Пустынный Этаж\n");
+        printf("4) Выход из игры\n");
+
         int choice;
         scanf("%d", &choice);
 
-        if (choice < 1 || choice > 3) {
-            printf("Неверный выбор!\n");
-            continue;
+        if (choice == 4) {
+            break;
         }
 
-        Enemy enemy = generate_enemy(locations[choice - 1]);
-        fight_enemy(enemy, locations[choice - 1]);
+        Monster monster;
+        switch (choice) {
+            case 1:  // Ледяной этаж
+                monster.name = "Ледяной Миньон";
+                monster.health = rand() % 30 + 10;
+                monster.damage = rand() % 10 + 5;
+                monster.coin_reward = rand() % 10 + 5;
+                monster.effect_chance = 20;  // 20% шанс заморозить
+                break;
+            case 2:  // Огненный этаж
+                monster.name = "Огненный Элементаль";
+                monster.health = rand() % 30 + 15;
+                monster.damage = rand() % 15 + 5;
+                monster.coin_reward = rand() % 15 + 10;
+                monster.effect_chance = 30;  // 30% шанс горения
+                break;
+            case 3:  // Пустынный этаж
+                monster.name = "Пустынный Монстр";
+                monster.health = rand() % 40 + 20;
+                monster.damage = rand() % 20 + 5;
+                monster.coin_reward = rand() % 20 + 15;
+                monster.effect_chance = 10;  // 10% шанс на убегание
+                break;
+            default:
+                printf(RED "Неверный выбор!\n" RESET);
+                continue;
+        }
+
+        fight_monster(monster);
+    }
+}
+
+void execute_command(char *command) {
+    char *args[MAX_ARGS];
+    int arg_count = 0;
+
+    // Разделение строки на аргументы
+    char *token = strtok(command, " ");
+    while (token != NULL && arg_count < MAX_ARGS - 1) {
+        args[arg_count++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[arg_count] = NULL;
+    if (arg_count == 0) {
+        return;
+    }
+
+    // Вызов игры
+    if (strcmp(args[0], "start_game") == 0) {
+        if (!game_running) {
+            start_game();
+        } else {
+            printf(RED "Игра уже идет! Завершите текущую игру перед началом новой.\n" RESET);
+        }
+        return;
+    }
+
+    // Если команда не является игрой, выполняем обычную команду
+    pid_t pid = fork();
+    if (pid == 0) {
+        execvp(args[0], args);
+        perror("Ошибка выполнения команды");
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) {
+        int status;
+        waitpid(pid, &status, 0);
+    } else {
+        perror("Ошибка создания процесса");
     }
 }
 
 int main() {
-    srand(time(NULL));
-    initialize_commands();
+    char input[MAX_INPUT];
+    FILE *file = fopen("logo.txt", "r");
+    char line[256];
 
-    printf("Добро пожаловать в текстовую игру Dungeon Crawler!\n");
+    // Вывод логотипа
+    while (fgets(line, sizeof(line), file)) {
+        printf(YELLOW "%s" RESET, line);
+    }
+    fclose(file);
 
-    dungeon_crawler();
+    // Основной цикл командной оболочки
+    while (1) {
+        printf(CYAN "kz_shell> " RESET);
+        if (fgets(input, MAX_INPUT, stdin) == NULL) {
+            break;
+        }
+
+        // Удаляем символ новой строки
+        input[strcspn(input, "\n")] = '\0';
+        if (strcmp(input, "exit") == 0) {
+            break;
+        }
+
+        execute_command(input);
+    }
     return 0;
 }
